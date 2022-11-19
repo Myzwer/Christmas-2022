7508a5cd3fc23574a604197fd931359dfacdcffc
Myzwer <josh@onefortyfivedesign.com>
2022-11-19 01:29:03 -0500

    :rocket: Finished uhh... all the other sections. Kind of a big commit here.

--- frontpage.php
+++ frontpage.php
@@ -150,7 +150,7 @@ get_header(); ?>
     <!-- End Service Info -->
 
     <!-- Start Split Section 1 -->
-    <div id = "experience" class="grid grid-cols-12">
+    <div id="experience" class="grid grid-cols-12">
         <div class="col-span-12 md:col-span-6">
             <div class="bg-no-repeat bg-scroll bg-cover relative img-cards" style="background:
                     url('<?php the_field('img_1'); ?>') no-repeat center center scroll; background-size: cover;">
@@ -176,7 +176,8 @@ get_header(); ?>
             <div class="content-middle-medium">
                 <div class="my-10 mx-5 md:mx-10">
                     <h2 class="text-left uppercase text-xl font-bold">Free Cookies</h2>
-                    <p>Moonshine Mountain Cookies are a gourmet cookie bakery located right here in East Tennessee. Their handcrafted small-batch cookies will keep you coming back for more!
+                    <p>Moonshine Mountain Cookies are a gourmet cookie bakery located right here in East Tennessee.
+                        Their handcrafted small-batch cookies will keep you coming back for more!
                     </p>
                 </div>
             </div>
@@ -202,7 +203,8 @@ get_header(); ?>
             <div class="content-middle-medium">
                 <div class="my-10 mx-5 md:mx-10">
                     <h2 class="text-left uppercase text-xl font-bold">Hot Chocolate Bar</h2>
-                    <p>Nothing says "Christmas Season" like a warm cup of hot chocolate and some peppermint sticks. You won't want to miss this special treat at A Foothills Christmas.
+                    <p>Nothing says "Christmas Season" like a warm cup of hot chocolate and some peppermint sticks. You
+                        won't want to miss this special treat at A Foothills Christmas.
                     </p>
                 </div>
             </div>
@@ -229,6 +231,141 @@ get_header(); ?>
         </div>
     </div>
     <!-- End Split Section 4 -->
+
+    <!-- Start FTG Section -->
+    <div class="grid grid-cols-12">
+        <div class="col-span-12 bg-green text-white relative ">
+            <div class=" lg:max-w-6xl lg:text-center lg:mx-auto pt-10 p-5">
+                <div class="my-10 mx-5 md:mx-10">
+                    <h2 class="uppercase text-2xl font-bold pb-5">First time at Foothills Church?</h2>
+                    <p class="text-left">We know going to a new church can be intimidating, but we promise to do
+                        whatever it takes to show
+                        you and your family that you belong here at FC this Christmas. We would love to get to know you
+                        and answer any questions you might have! Scroll down to find directions to both of our locations
+                        and to see some of our Frequently Asked Questions. We hope to see you and your family this
+                        Christmas!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End FTG Section -->
+
+    <!-- Start Directions Section -->
+    <div class="grid grid-cols-12">
+        <div class="col-span-12 md:col-span-6">
+            <div class="bg-no-repeat bg-scroll bg-cover relative img-cards" style="background:
+                    url('<?php the_field('map_image'); ?>') no-repeat center center scroll; background-size: cover;">
+            </div>
+        </div>
+
+        <div class="col-span-12 md:col-span-6 bg-yellow text-black relative">
+            <div class="content-middle-medium">
+                <div class="my-10 mx-5 md:mx-10">
+                    <h2 class="text-left uppercase text-xl font-bold">Directions</h2>
+                    <div class="my-3">
+                        <h3 class="font-bold text-lg">Maryville Location</h3>
+                        <p>1551 W. Lamar Alexander Pkwy</p>
+                        <p>Maryville Tn, 37801</p>
+                    </div>
+                    <a
+                            href="#">
+                        <button class="mx-auto lg:mx-0 bg-green text-white font-bold rounded-md py-3 px-5 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                            Launch In Maps
+                        </button>
+                    </a>
+                </div>
+
+                <div class="my-10 mx-5 md:mx-10">
+                    <div class="my-3">
+                        <h3 class="font-bold text-lg">Maryville Location</h3>
+                        <p>1551 W. Lamar Alexander Pkwy</p>
+                        <p>Maryville Tn, 37801</p>
+                    </div>
+                    <a
+                            href="#">
+                        <button class="mx-auto lg:mx-0 bg-green text-white font-bold rounded-md py-3 px-5 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                            Launch In Maps
+                        </button>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- End Directions Section -->
+
+    <!-- Start Service Info -->
+    <div class="bg-no-repeat bg-scroll bg-cover relative" style="background:
+            url('<?php the_field('noise_background'); ?>') no-repeat bottom center scroll; background-size: cover; ">
+        <div class=" lg:max-w-6xl lg:mx-auto pt-10 p-5">
+            <h1 class="text-center uppercase text-black text-2xl">FAQ</h1>
+
+            <div class="grid grid-cols-12 gap-4 lg:gap-4 py-4 text-black mb-10">
+
+                <div class="col-span-12 md:col-span-6 mt-5">
+                    <h2 class="uppercase text-black text-xl font-bold">WHERE DO I GO WHEN I GET THERE?
+                    </h2>
+                    <p>If it's your first time visiting us, our volunteers will direct you to our VIP Parking. We have a
+                        special parking reserved for you. As you arrive on campus, just follow the signs to our VIP lot.
+                        From there, one of our volunteers will direct you to our VIP tent, where a Guest Experience
+                        volunteer will meet with you and help get started.
+                    </p>
+                </div>
+
+                <div class="col-span-12 md:col-span-6 mt-5">
+                    <h2 class="uppercase text-black text-xl font-bold">WHAT DO I WEAR?
+
+                    </h2>
+                    <p>Here at FC we encourage you to come as you are. There is no dress code, but most of our partners
+                        wear casual attire. Please feel free to wear what makes you comfortable!
+                    </p>
+                </div>
+
+
+                <div class="col-span-12 md:col-span-6 mt-5">
+                    <h2 class="uppercase text-black text-xl font-bold">HOW LONG DOES THE WORSHIP EXPERIENCE LAST?
+
+                    </h2>
+                    <p>Our worship experience usually lasts approximately 70 minutes. We encourage you to arrive early
+                        to grab some coffee and check your children into one of our FC Kids environments.
+
+                    </p>
+                </div>
+
+                <div class="col-span-12 md:col-span-6 mt-5">
+                    <h2 class="uppercase text-black text-xl font-bold">WHAT DO I DO IF I HAVE QUESTIONS?
+
+                    </h2>
+                    <p>When you arrive on campus, our Guest Experience volunteers at the black VIP tents are equipped to
+                        answer any questions you may have! However, you can also go to the Connect Center located in the
+                        Atrium if you need to talk to someone. Finally, you can email us at info@foothillschurch.com
+                        during the week with any other questions you may have.
+                    </p>
+                </div>
+
+                <div class="col-span-12 md:col-span-6 mt-5">
+                    <h2 class="uppercase text-black text-xl font-bold">CAN I ATTEND IF I’M ____?
+                    </h2>
+                    <p>Fill in the blank with any fear you might have about not being accepted. You belong here. So come as you are, and we’ll see you at A Foothills Christmas!
+
+
+                    </p>
+                </div>
+
+                <div class="col-span-12 md:col-span-6 mt-5">
+                    <h2 class="uppercase text-black text-xl font-bold">CAN I WATCH ONLINE?
+
+                    </h2>
+                    <p>If you’re going to be out of town, or if you live somewhere else and you won’t be able to make it to A Foothills Christmas, that’s okay! You can still worship online by going to our YouTube on December 23 at 5pm or December 24th at 2pm or 5pm.
+
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- End Service Info -->
 
 <?php
 get_footer();
